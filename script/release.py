7224f749be3d365960bfd2df3c489647912582b5
Lyr-7D1h <lyr-7d1h@pm.me>
2022-08-05 16:02:33 +0200

    modularized release script

--- script/release.py
+++ script/release.py
@@ -4,18 +4,20 @@ from abc import ABCMeta, abstractmethod
 from genericpath import exists
 from os import chdir
 import os
+import re
 import subprocess
 import sys
-from typing import Literal, NoReturn
-
-HOME = os.environ["HOME"]
-GitRepository = Literal["github.com"]
-Project = Literal["cargo"]
+from typing import Dict, List, NoReturn, Optional, Tuple
+from urllib.request import Request, urlopen
 
 
 def error(*args) -> NoReturn:
     print("\033[91m" + " ".join(args) + "\033[0m", file=sys.stderr)
     exit(1)
+
+
+def warn(*args) -> None:
+    print("\033[92m" + " ".join(args) + "\033[0m", file=sys.stderr)
 
 
 def info(*args: str) -> None:
@@ -32,60 +34,122 @@ def exec(command: str) -> str:
     return res.stdout.decode("utf-8").rstrip()
 
 
-# class Module(metaclass=ABCMeta):
-#     @abstractmethod
-#     def should_load(self) -> bool:
-#         "Should this module load"
-
-#     @abstractmethod
-#     def validate(self) -> None:
-#         """Validate that the module has everything it needs to release"""
-
-#     @abstractmethod
-#     def release(self) -> None:
-#         """Make release on given module"""
+def request(url: str, headers: Optional[Dict[str, str]] = None) -> Tuple[int, str]:
+    if headers == None:
+        headers = {}
+    req = Request(url, headers=headers)
+    with urlopen(req) as response:
+        return response.status, str(response.read())
 
 
-# class Github(Module):
-#     def should_load(self):
-#         url = exec("git remote get-url origin")
-#         return "github.com" in url
+HOME = os.environ["HOME"]
+VERSION = sys.argv[1]
+pattern = "^[0-9]*\\.[0-9]*\\.[0-9]*$"
+if re.match(pattern, VERSION) == None:
+    error("No version given")
 
 
-def get_git_repository() -> GitRepository:
-    url = exec("git remote get-url origin")
-    if "github.com" in url:
-        return "github.com"
-    error("Could not find repository from", url)
+class Module(metaclass=ABCMeta):
+    def name(self) -> str:
+        return type(self).__name__
+
+    @abstractmethod
+    def should_load(self) -> bool:
+        "Should this module load"
+
+    @abstractmethod
+    def link(self) -> Optional[str]:
+        """Give a link to the project"""
+
+    @abstractmethod
+    def validate(self) -> None:
+        """Validate that the module has everything it needs to release"""
+
+    def pre_release(self) -> None:
+        pass
+
+    @abstractmethod
+    def release(self) -> None:
+        """Make release on given module"""
 
 
-def get_project() -> Project:
-    if exists("Cargo.toml"):
-        return "cargo"
-    error("Could not find what kind of project this is")
+class Github(Module):
+    def should_load(self):
+        url = exec("git remote get-url origin")
+        return "github.com" in url
+
+    def link(self):
+        return exec("gh browse -n")
+
+    def validate(self):
+        exec("gh auth status")
+
+    def release(self):
+        exec(f"gh release create {VERSION}")
 
 
-def prepare_branch() -> None:
-    master_branch = "master"
+class Aur(Module):
+    def should_load(self):
+        url = exec("git remote get-url origin")
+        exec("git --no-pager shortlog -sne")
+
+    def validate(self):
+        exec("gh auth status")
+
+    def release(self):
+        exec(f"gh release create {VERSION}")
+
+
+class Cargo(Module):
+    def should_load(self):
+        return exists("Cargo.toml")
+
+    def link(self):
+        with open("Cargo.toml", "r") as file:
+            name = re.search("^name\\s*=.*", file.read(), re.MULTILINE)
+            if name is None:
+                error("Could not find crate name")
+            name = str(name).split("=")[1].replace(" ", "")
+
+        link = f"https://crates.io/crates/{name}"
+        [_, content] = request(link, {"Accept": "text/html"})
+
+        if "Not Found" in content:
+            warn(f"{link} does not exist")
+
+        return
+
+    def validate(self):
+        if not exists(f"{HOME}/.cargo/credentials"):
+            error(f"{HOME}/.cargo/credentials does not exist")
+
+        exec("cargo test")
+        exec("cargo build --release --locked")
+        # exec("cargo publish --dry-run")
+
+    def pre_release(self) -> None:
+        info("Updating version in Cargo.toml")
+        with open("Cargo.toml", "r") as file:
+            cargo_toml: str = file.read()
+        cargo_toml = re.sub("^version\\s*=.*", f'version = "{VERSION}"', cargo_toml)
+        with open("Cargo.toml", "w") as file:
+            file.write(cargo_toml)
+        exec("git add Cargo.toml")
+
+    def release(self):
+        exec(f"cargo publish")
+
+
+def prepare_branch():
+    """Returns the remote"""
+    branch = "master"
     if "master" not in exec("git branch"):
-        master_branch = "main"
-    remote = exec(f"git config branch.{master_branch}.remote")
+        branch = "main"
+    remote = exec(f"git config branch.{branch}.remote")
 
-    exec(f"git switch {master_branch}")
-    exec(f"git pull {remote} {master_branch}")
-
-
-def validate_github():
-    exec("gh auth status")
-
-
-def validate_cargo():
-    if not exists(f"{HOME}/.cargo/credentials"):
-        error(f"{HOME}/.cargo/credentials does not exist")
-
-    exec("cargo test")
-    exec("cargo build --release --locked")
-    exec("cargo publish --dry-run")
+    exec(f"git switch {branch}")
+    exec(f"git pull {remote} {branch}")
+    return remote
 
 
 def release():
@@ -93,19 +157,34 @@ def release():
     info(f"Moving to root '{root}'")
     chdir(root)
 
-    repo = get_git_repository()
-    project = get_project()
-
     # if exec("git status --short") != "":
     #     error("Git branch is dirty")
 
     prepare_branch()
 
-    if repo == "github.com":
-        validate_github()
+    modules: List[Module] = []
+    for module in [Github(), Cargo()]:
+        if module.should_load():
+            info(f"Found {module.name()}")
+            modules.append(module)
 
-    if project == "cargo":
-        validate_cargo()
+    for module in modules:
+        module.validate()
+
+    for module in modules:
+        info(f"Will release on {module.name()} ({module.link()})")
+
+    answer = input("Proceed with release? [Y/n]")
+    if answer.lower() != "y":
+        error("")
+
+    # for module in modules:
+    #     module.pre_release()
+
+    # exec(f"git tag {VERSION}")
+    # exec(f"git push {remote} --tags")
+    # for module in modules:
+    #     module.release()
 
 
 release()
