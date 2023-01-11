b27c13a3f07aa912d4a44cf42ef6b663904a50c9
Lyr <lyr-7d1h@pm.me>
2023-01-11 20:43:03 +0100

    refactor: moved default config path to args.rs

--- src/args.rs
+++ src/args.rs
@@ -10,10 +10,15 @@ pub struct Args {
     pub deduplicate: bool,
 }
 
+/// Get the xdg default config path
+fn default_config_path() -> Option<PathBuf> {
+    Some(dirs::config_dir()?.join("sworkstyle/config.toml"))
+}
+
 impl Args {
     pub fn from_cli() -> Args {
         let mut log_level = LevelFilter::Warn;
-        let mut config_path = None;
+        let mut config_path = default_config_path();
         let mut deduplicate = false;
 
         let mut args = env::args().skip(1);
@@ -39,7 +44,7 @@ FLAGS
         Either \"error\", \"warn\", \"info\", \"debug\", \"off\". Uses \"warn\" by default
         
     -c, --config 
-        Specifies the config file to use.
+        Specifies the config file to use. Uses \"`XDG_CONFIG_HOME`/sworkstyle/config\" by default
 
     -d, --deduplicate
         Deduplicate the same icons in your workspace
@@ -95,7 +100,7 @@ FLAGS
         return Args {
             log_level,
             config_path,
-            deduplicate
+            deduplicate,
         };
     }
 }
--- src/config.rs
+++ src/config.rs
@@ -2,9 +2,9 @@ extern crate dirs;
 
 use std::{fs::read_to_string, path::Path};
 
-use log::{debug, error};
+use log::{debug, warn};
 
-use self::{config_error::ConfigError, match_config::MatchConfig};
+use self::match_config::MatchConfig;
 
 mod config_error;
 mod match_config;
@@ -15,59 +15,25 @@ pub struct Config {
     match_config: MatchConfig,
 }
 
-/// Fetch user config content at path given otherwise return config from default location
-///
-/// returns `None` if there is no config at the default location
-fn get_user_config_content<P: AsRef<Path>>(
-    config_path: Option<P>,
-) -> Result<Option<String>, ConfigError> {
-    if let Some(path) = config_path {
-        let path = path.as_ref();
-        match read_to_string(&path) {
-            Ok(content) => return Ok(Some(content)),
-            Err(e) => error!(
-                "Could not open config path: {}, {}",
-                path.to_string_lossy(),
-                e
+fn match_config<P: AsRef<Path>>(config_path: Option<P>) -> MatchConfig {
+    if let Some(config_path) = config_path {
+        match read_to_string(&config_path) {
+            Ok(content) => return MatchConfig::from(content),
+            Err(e) => debug!(
+                "Could not create config from path: {:?} {e}",
+                config_path.as_ref()
             ),
         }
+    } else {
+        warn!("Default config could not have been found")
     }
 
-    let sworkstyle_config_dir = match dirs::config_dir() {
-        Some(dir) => dir.join("sworkstyle"),
-        None => return Err(ConfigError::new("Could not find config directory")),
-    };
-
-    let sworkstyle_config_path = sworkstyle_config_dir.join("config.toml");
-
-    if !sworkstyle_config_path.exists() {
-        return Ok(None);
-    }
-
-    match read_to_string(&sworkstyle_config_path) {
-        Ok(content) => Ok(Some(content)),
-        Err(e) => Err(ConfigError::new(&format!(
-            "Could not open {sworkstyle_config_path:?}: {e}"
-        ))),
-    }
+    return MatchConfig::default();
 }
 
 impl Config {
     pub fn new<P: AsRef<Path>>(config_path: Option<P>) -> Config {
-        let match_config = match get_user_config_content(config_path) {
-            Ok(c) => {
-                if let Some(c) = c {
-                    MatchConfig::from(c)
-                } else {
-                    debug!("No config found, using default");
-                    MatchConfig::default()
-                }
-            }
-            Err(e) => {
-                error!("Failed to create config: {e}");
-                MatchConfig::default()
-            }
-        };
+        let match_config = match_config(config_path);
 
         Config { match_config }
     }
